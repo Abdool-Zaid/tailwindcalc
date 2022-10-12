7f63dbddc5047e5e35b53138162ab2b546a7889b
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-12 12:59:20 +0200

    'break'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+function loadFunc() {
+  let center = document.querySelector("#centerButton").getBoundingClientRect();
+  let xCoor = center.x;
+  let yCoor = center.y;
+  let width = center.width / 2;
+  let height = center.height / 2;
+  let centerCoor = { x: xCoor + width, y: yCoor + height };
+  // document.querySelectorAll('*').forEach(Element=>{
+  //   Element.style=`
+  // position: fixed;
+  // left: ${centerCoor.x};
+  // top:${centerCoor.y} ;`
+  // })
+
+  console.log(document.querySelectorAll("*").length);
+  return centerCoor;
+}
+function getElementID(event){
+  let id ='#'+event.target.id
+  return id
+}
+function defineRadials(event) {
+  console.log(getElementID(event))
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <button @click="loadFunc()">//onload</button>
+  <div class="flex flex-col" id="main">
+    <div class="flex">
+      <div
+        id="headDiv"
+        class="bg-slate-400 rounded-full aspect-square w-1/2"
+        @click="defineRadials($event)"
+      ></div>
+      <div id="tearsleft" class="z-10"></div>
+      <div id="tearsRight" class="z-10"></div>
     </div>
-  </header>
-
-  <RouterView />
+    <div
+      id="bodyDiv"
+      class="valuesPrimitive bg-slate-400 rounded-full aspect-square"
+    >
+      <div class="valuesPrimitive bg-slate-400">
+        <button>0</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
+      <div class="operators bg-slate-400">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+      </div>
+      <div class="operatorsAdvance bg-slate-400">
+        <button>^</button>
+        <button>(</button>
+        <button>)</button>
+        <button>√</button>
+        <button>π</button>
+      </div>
+      <button class="rounded-full bg-slate-200 aspect-square" id="centerButton">
+        =
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -32,10 +89,9 @@ header {
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  position: fixed;
+  left: ;
+  top: ;
 }
 
 nav a.router-link-exact-active {
